4a6f3e8ebcc51645b702e8e13febc820fef54822
erin-yiran-wang <103444093+erinwang27y@users.noreply.github.com>
2022-11-09 21:17:03 -0600

    connected to our database

--- app/_init_.py
+++ app/_init_.py
@@ -6,42 +6,52 @@ from yaml import load, Loader
 
 
 
-# def init_connection_engine():
-#     """ initialize database setup
-#     Takes in os variables from environment if on GCP
-#     Reads in local variables that will be ignored in public repository.
-#     Returns:
-#         pool -- a connection to GCP MySQL
-#     """
+def init_connection_engine():
+    """ initialize database setup
+    Takes in os variables from environment if on GCP
+    Reads in local variables that will be ignored in public repository.
+    Returns:
+        pool -- a connection to GCP MySQL
+    """
 
 
-#     # detect env local or gcp
-#     if os.environ.get('GAE_ENV') != 'standard':
-#         try:
-#             variables = load(open("app.yaml"), Loader=Loader)
-#         except OSError as e:
-#             print("Make sure you have the app.yaml file setup")
-#             os.exit()
+    # detect env local or gcp
+    if os.environ.get('GAE_ENV') != 'standard':
+        # try:
+        variables = load(open("app.yaml"), Loader=Loader)
+        # except OSError as e:
+        #     print("Make sure you have the app.yaml file setup")
+        #     os.exit()
 
-#         env_variables = variables['env_variables']
-#         for var in env_variables:
-#             os.environ[var] = env_variables[var]
+        env_variables = variables['env_variables']
+        try:
+            for var in env_variables:
+                os.environ[var] = env_variables[var]
+        except TypeError as e:
+            print("error", var)
+            raise e
 
-#     pool = sqlalchemy.create_engine(
-#         sqlalchemy.engine.url.URL(
-#             drivername="mysql+pymysql",
-#             username=os.environ.get('MYSQL_USER'),
-#             password=os.environ.get('MYSQL_PASSWORD'),
-#             database=os.environ.get('MYSQL_DB'),
-#             host=os.environ.get('MYSQL_HOST')
-#         )
-#     )
+    pool = sqlalchemy.create_engine(
+        sqlalchemy.engine.url.URL(
+            drivername="mysql+pymysql",
+            username=os.environ.get('MYSQL_USER'),
+            password=os.environ.get('MYSQL_PASSWORD'),
+            database=os.environ.get('MYSQL_DB'),
+            host=os.environ.get('MYSQL_HOST')
+        )
+    )
 
-#     return pool
+    return pool
 
 
 app = Flask(__name__)
-# db = init_connection_engine()
+db = init_connection_engine()
+
+# conn = db.connect()
+# results = conn.execute("Select * from Company")
+# # we do this because results is an object, this is just a quick way to verify the content
+# print([x for x in results])
+# conn.close()
 
 # To prevent from using a blueprint, we use a cyclic import
 # This also means that we need to place this import here
